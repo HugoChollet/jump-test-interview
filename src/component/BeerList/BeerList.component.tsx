d90a05f54259e05d57fc254f7acc81b6d6b6bd39
HugoChollet <tibolescabo@gmail.com>
2023-05-01 23:09:24 +0200

    create filter for beerList

--- src/component/BeerList/BeerList.component.tsx
+++ src/component/BeerList/BeerList.component.tsx
@@ -1,30 +1,43 @@
 import { Button, ButtonGroup } from "@mui/material";
 import { useRouter } from "next/router";
 
-export const BeerList = ({ items }: any) => {
+export const BeerList = ({
+  items,
+  filter,
+}: {
+  items: Array<any>;
+  filter: string;
+}) => {
   const router = useRouter();
 
   return (
     <ButtonGroup orientation="vertical">
-      {items.map((item: any) => (
-        <Button
-          key={item.id}
-          variant={"outlined"}
-          onClick={() => {
-            router.push(
-              {
-                pathname: "/details",
-                query: {
-                  data: JSON.stringify(item),
-                },
-              },
-              "/details"
-            );
-          }}
-        >
-          {item.name}
-        </Button>
-      ))}
+      {items.map((item: any) => {
+        if (
+          item.name.toLowerCase().search(filter.toLowerCase()) !== -1 ||
+          filter === ""
+        ) {
+          return (
+            <Button
+              key={item.id}
+              variant={"outlined"}
+              onClick={() => {
+                router.push(
+                  {
+                    pathname: "/details",
+                    query: {
+                      data: JSON.stringify(item),
+                    },
+                  },
+                  "/details"
+                );
+              }}
+            >
+              {item.name}
+            </Button>
+          );
+        }
+      })}
     </ButtonGroup>
   );
 };
--- src/pages/homeContent.tsx
+++ src/pages/homeContent.tsx
@@ -2,15 +2,18 @@ import { Inter } from "next/font/google";
 import { BeerList } from "@/component/BeerList/BeerList.component";
 import { RandomCards } from "@/component/RandomCards/RandomCards";
 import { useQuery } from "react-query";
+import { TextField } from "@mui/material";
+import { useState } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function HomeContent() {
   const { isLoading, error, data } = useQuery("repoData", () =>
-    fetch("https://api.punkapi.com/v2/beers?per_page=10").then((res) =>
+    fetch("https://api.punkapi.com/v2/beers?per_page=20").then((res) =>
       res.json()
     )
   );
+  const [filterSearch, setFilterSearch] = useState("");
 
   if (isLoading) return "Loading...";
 
@@ -19,7 +22,16 @@ export default function HomeContent() {
   return (
     <div>
       <RandomCards beerData={data} />
-      <BeerList items={data} />
+      <TextField
+        id="filled-basic"
+        label="Searched beer"
+        variant="standard"
+        placeholder="BLONDE"
+        onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+          setFilterSearch(event.target.value);
+        }}
+      />
+      <BeerList items={data} filter={filterSearch} />
     </div>
   );
 }
