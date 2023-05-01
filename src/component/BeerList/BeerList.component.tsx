3a4d4b56445d8cfdd92353e94e4d0795fd7c6dbb
HugoChollet <tibolescabo@gmail.com>
2023-05-01 22:26:51 +0200

    add navigation for beerList

--- src/component/BeerList/BeerList.component.tsx
+++ src/component/BeerList/BeerList.component.tsx
@@ -1,10 +1,27 @@
 import { Button, ButtonGroup } from "@mui/material";
+import { useRouter } from "next/router";
 
 export const BeerList = ({ items }: any) => {
+  const router = useRouter();
+
   return (
     <ButtonGroup orientation="vertical">
       {items.map((item: any) => (
-        <Button key={item.id} variant={"outlined"}>
+        <Button
+          key={item.id}
+          variant={"outlined"}
+          onClick={() => {
+            router.push(
+              {
+                pathname: "/details",
+                query: {
+                  data: JSON.stringify(item),
+                },
+              },
+              "/details"
+            );
+          }}
+        >
           {item.name}
         </Button>
       ))}
